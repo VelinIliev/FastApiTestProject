72fdc28eb8a094ea24d1b2073550896033dd7067
VelinIliev <veliniliev@gmail.com>
2023-11-01 10:09:55 +0200

    update

---
+++ TodoApp/routers/auth.py
@@ -0,0 +1,118 @@
+from datetime import timedelta, datetime
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
+from models import Users
+
+SECRET_KEY = '58252c5fa9e7e526603fa6181b613f37075ebaedafb4d50a6e626a3f16cde2e1'
+ALGORITHM = 'HS256'
+
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oath2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.utcnow() + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oath2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if not username or not user_id:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Could not validate user.')
+        return {'username': username, 'id': user_id}
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Could not validate user.')
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.password):
+        return False
+    return user
+
+
+class UserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+@router.get('/')
+async def get_users(db: db_dependency):
+    return db.query(Users).all()
+
+
+@router.post('/create', status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: UserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        password=bcrypt_context.hash(create_user_request.password),
+        role=create_user_request.role,
+        is_active=True
+    )
+    db.add(create_user_model)
+    db.commit()
+
+
+@router.post('/token', response_model=Token)
+async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Could not validate user.')
+
+    token = create_access_token(user.username, user.id, timedelta(minutes=30))
+
+    return {'access_token': token, 'token_type': 'bearer'}
--- books.py
+++ books.py
@@ -1,7 +1,8 @@
 from typing import Optional
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query, HTTPException
 from pydantic import BaseModel, Field
+from starlette import status
 
 app = FastAPI()
 
@@ -22,7 +23,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
-    published_date: int = Field(gt=1800)
+    published_date: int = Field(gt=1800, lt=2030)
 
     class Config:
         json_schema_extra = {
@@ -46,7 +47,7 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def view_books():
     return BOOKS
 
@@ -56,15 +57,16 @@ def find_book_id(book: Book):
     return book
 
 
-@app.get("/books/{book_id}")
-async def view_book(book_id: int):
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def view_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail='Book not found')
 
 
-@app.get("/books/")
-async def get_books_by_rating(rating: int):
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def get_books_by_rating(rating: int = Query(gt=0, lt=6)):
     books = []
     for book in BOOKS:
         if book.rating == rating:
@@ -72,8 +74,8 @@ async def get_books_by_rating(rating: int):
     return books
 
 
-@app.get("/books/publish/")
-async def get_books_by_year(year: int):
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
+async def get_books_by_year(year: int = Query(gt=1800, lt=2030)):
     books = []
     for book in BOOKS:
         if book.published_date == year:
@@ -81,22 +83,30 @@ async def get_books_by_year(year: int):
     return books
 
 
-@app.post("/create-book")
+@app.post("/create-book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_data: BookRequest):
     new_book = Book(**book_data.model_dump())
     BOOKS.append(find_book_id(new_book))
 
 
-@app.put("/books/update_book")
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Book not found.')
 
 
-@app.delete("/books/{book_id}/delete")
-async def delete_book(book_id: int):
+@app.delete("/books/{book_id}/delete", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Book not found.')
